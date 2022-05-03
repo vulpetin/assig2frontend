f5c239424ddf2b55f0447323b469a80368133de8
Valentin Vulpe <vulpetin@gmail.com>
2022-05-03 23:57:33 +0300

    assig3

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,6 +1,26 @@
 import {Link, Outlet, useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import useToken from "../login/useToken";
+// import {
+//     Document,
+//     Page,
+//     Text,
+//     View,
+//     StyleSheet,
+//     PDFViewer,
+// } from "@react-pdf/renderer";
+
+// const styles = StyleSheet.create({
+//     page: {
+//         flexDirection: 'row',
+//         backgroundColor: '#E4E4E4'
+//     },
+//     section: {
+//         margin: 10,
+//         padding: 10,
+//         flexGrow: 1
+//     }
+// });
 
 
 export default function Menu () {
@@ -28,37 +48,44 @@ export default function Menu () {
                                 <br/><br/>
                                 <h3>Menu</h3><br/>
                                 <nav>
-                                    <table className="table table-bordered">
-                                        <tr>
-                                            <th>No</th>
-                                            <th>id</th>
-                                            <th>Name</th>
-                                            <th>Description</th>
-                                            <th>Price</th>
-                                            <th>Category</th>
-                                        </tr>
-                                        {
-                                            foods ?
+                                    <div>
+                                        <table className="table table-bordered">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>id</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Price</th>
+                                                <th>Category</th>
+                                            </tr>
+                                            {
+                                                foods ?
 
-                                                foods.map((food, i) =>
-                                                    <tr>
-                                                        <td>{++i}</td>
-                                                        <td>{food.id}</td>
-                                                        <td>{food.name}</td>
-                                                        <td>{food.description}</td>
-                                                        <td>{food.price}</td>
-                                                        <td>{food.category}</td>
-                                                    </tr>
-                                                )
-                                                :
-                                                null
-                                        }
-                                    </table>
+                                                    foods.map((food, i) =>
+                                                        <tr>
+                                                            <td>{++i}</td>
+                                                            <td>{food.id}</td>
+                                                            <td>{food.name}</td>
+                                                            <td>{food.description}</td>
+                                                            <td>{food.price}</td>
+                                                            <td>{food.category}</td>
+                                                        </tr>
+                                                    )
+                                                    :
+                                                    null
+                                            }
+                                        </table>
+                                    </div>
+                                    <div>
+
+                                    </div>
+
+
+
                                 </nav>
-                                <Outlet/>
                             </div>
                         </div>
                     </div>
                 </div>
-            )
+            );
 }
